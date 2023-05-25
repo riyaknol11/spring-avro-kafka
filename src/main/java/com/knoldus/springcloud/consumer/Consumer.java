d640625e70549af8f5baf0d583da955c5f7f4e64
knoldus <riya.sharma@knoldus.com>
2023-05-25 18:48:36 +0530

    "Kafka Configuration with Confluent Cloud and schema registry"

--- src/main/java/com/knoldus/springcloud/consumer/Consumer.java
+++ src/main/java/com/knoldus/springcloud/consumer/Consumer.java
@@ -1,13 +1,42 @@
 package com.knoldus.springcloud.consumer;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class Consumer {
+    private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+    public ConsumerFactory<String, byte[]> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "xrnwx.asia-south2.gcp.confluent.cloud:9092");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "spring-boot-kafka");
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    public ConcurrentKafkaListenerContainerFactory<String, byte[]> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, byte[]> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
     @KafkaListener(topics = {"hobbit"}, groupId = "spring-boot-kafka")
-    public void consume(ConsumerRecord<Integer, String> record){
-        System.out.println("received : " + record.value() + "for the key " + record.key());
+    public void consume(ConsumerRecord<Integer, byte[]> record) {
+        log.info(String.format("Received: value='%s' for the key='%d'", new String(record.value()), record.key()));
     }
 }
+
